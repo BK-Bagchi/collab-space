a1e9bae2ec23d508ce2e725d69caaecf7a5df6f6
unknown <bkbagchi.dipto@gmail.com>
2025-10-13 18:29:31 +0600

    index import file added to make all api(s) import clean

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+export * as AuthAPI from "./auth";
+export * as UserAPI from "./user";
+export * as ProjectAPI from "./project";
+export * as TaskAPI from "./task";
+export * as ChatAPI from "./chat";
+export * as NotificationAPI from "./notification";
+export * as FileAPI from "./file";
+export * as DashboardAPI from "./dashboard";
+export * as SearchAPI from "./search";
+
+// later use in react
+// import { ProjectAPI } from "@/api";
+// const { data } = await ProjectAPI.getUserProjects();
+// console.log(data);
