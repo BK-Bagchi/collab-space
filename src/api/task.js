79837e841f71375b78155dbabeb118dfae5ec901
unknown <bkbagchi.dipto@gmail.com>
2025-10-13 17:37:50 +0600

    task endpoint api(s) defined

---
+++ src/api/task.js
@@ -0,0 +1,16 @@
+import axios from "./axiosInstance";
+
+export const createTask = (data) => axios.post("/tasks", data);
+export const getTaskById = (id) => axios.get(`/tasks/${id}`);
+export const updateTask = (id, data) => axios.put(`/tasks/${id}`, data);
+export const deleteTask = (id) => axios.delete(`/tasks/${id}`);
+export const getProjectTasks = (id, params = {}) =>
+  axios.get(`/projects/${id}/tasks`, { params });
+export const updateSubtasks = (id, data) =>
+  axios.patch(`/tasks/${id}/subtasks`, data);
+export const updateTaskStatus = (id, data) =>
+  axios.patch(`/tasks/${id}/status`, data);
+export const uploadTaskAttachment = (id, formData) =>
+  axios.post(`/tasks/${id}/attachments`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
