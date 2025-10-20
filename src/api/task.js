8a40b301cd165d99251693bf51bbb6ecc8dd5552
unknown <bkbagchi.dipto@gmail.com>
2025-10-20 11:28:33 +0600

    api endpoint corrected

--- src/api/task.js
+++ src/api/task.js
@@ -1,18 +1,18 @@
 import axios from "./axiosInstance";
 
-export const createTask = (data) => axios.post("/tasks", data);
+export const createTask = (data) => axios.post("/task", data);
 export const assignedTaskToUser = () => axios.get("/task/user");
-export const getTaskById = (id) => axios.get(`/tasks/${id}`);
-export const updateTask = (id, data) => axios.put(`/tasks/${id}`, data);
-export const deleteTask = (id) => axios.delete(`/tasks/${id}`);
+export const getTaskById = (id) => axios.get(`/task/${id}`);
+export const updateTask = (id, data) => axios.put(`/task/${id}`, data);
+export const deleteTask = (id) => axios.delete(`/task/${id}`);
 export const getProjectTasks = (id, params = {}) =>
   axios.get(`/projects/${id}/tasks`, { params });
 export const updateSubtasks = (id, data) =>
-  axios.patch(`/tasks/${id}/subtasks`, data);
+  axios.patch(`/task/${id}/subtasks`, data);
 export const updateTaskStatus = (id, data) =>
-  axios.patch(`/tasks/${id}/status`, data);
+  axios.patch(`/task/${id}/status`, data);
 export const uploadTaskAttachment = (id, formData) =>
-  axios.post(`/tasks/${id}/attachments`, formData, {
+  axios.post(`/task/${id}/attachments`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 export const getTaskFiles = (id) => axios.get(`/task/${id}/files`);
