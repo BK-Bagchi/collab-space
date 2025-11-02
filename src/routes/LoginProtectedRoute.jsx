df5ac0b7c2caa839c9a10a6e282834cf2e580b1d
unknown <bkbagchi.dipto@gmail.com>
2025-11-02 16:58:12 +0600

    on logout user instantly force redirect to login page

--- src/routes/LoginProtectedRoute.jsx
+++ src/routes/LoginProtectedRoute.jsx
@@ -1,8 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 const LoginProtectedRoute = () => {
-  const token = localStorage.getItem("token");
-  if (!token) return <Navigate to="/login" replace />;
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen text-[#26A69A] font-semibold">
+        Checking authentication...
+      </div>
+    );
+  }
+
+  if (!user) return <Navigate to="/login" replace />;
 
   return <Outlet />;
 };
