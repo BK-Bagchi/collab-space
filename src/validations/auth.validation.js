61446969ad641a480797dc493b169ce80ee00e3a
unknown <bkbagchi.dipto@gmail.com>
2025-11-23 10:40:52 +0600

    password regex more strong in registration schema

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -9,10 +9,17 @@ export const registrationSchema = z
   .object({
     name: z.string().min(3, "Name must be at least 3 characters"),
     email: z.string().email("Invalid email address"),
-    password: z.string().min(8, "Password must be at least 8 characters"),
-    confirmPassword: z
+    password: z
       .string()
-      .min(8, "Password must be at least 8 characters"),
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Password must include at least one uppercase letter")
+      .regex(/[a-z]/, "Password must include at least one lowercase letter")
+      .regex(/[0-9]/, "Password must include at least one number")
+      .regex(
+        /[@$!%*?&#^(){}[\]=+._-]/,
+        "Password must include at least one special character"
+      ),
+    confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Password and Confirm Password do not match",
