819ed456dc8fd512cbfba756f8ae298cbccc96fe
unknown <bkbagchi.dipto@gmail.com>
2025-12-16 12:39:06 +0600

    api added to toggle theme, typing indicator and active status

--- src/api/user.js
+++ src/api/user.js
@@ -3,6 +3,11 @@ import axios from "./axiosInstance";
 export const getLoggedInUser = () => axios.get("/user/me");
 export const updateUserProfile = (data) => axios.put("/user/me", data);
 export const updatePassword = (data) => axios.patch("/user/me/password", data);
+export const toggleUserTheme = (data) => axios.patch("/user/me/theme", data);
+export const toggleUserTypingIndicator = (data) =>
+  axios.patch("/user/me/typing-indicator", data);
+export const toggleUserActiveStatus = (data) =>
+  axios.patch("/user/me/active-status", data);
 export const getAllUsers = () => axios.get("/user");
 export const getUserById = (id) => axios.get(`/user/${id}`);
 export const updateUserRole = (id, data) => axios.put(`/user/${id}/role`, data);
