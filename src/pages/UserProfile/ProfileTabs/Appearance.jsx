671c459a91cc43f254ff822deadcd6ac08a27831
unknown <bkbagchi.dipto@gmail.com>
2025-12-16 16:18:51 +0600

    user can toggle theme now

--- src/pages/UserProfile/ProfileTabs/Appearance.jsx
+++ src/pages/UserProfile/ProfileTabs/Appearance.jsx
@@ -1,9 +1,23 @@
 import { Moon } from "lucide-react";
+import { toast } from "react-toastify";
+import { useAuth } from "../../../hooks/useAuth";
 import { useSettings } from "../../../hooks/useSettings";
+import { UserAPI } from "../../../api";
 
 const Appearance = () => {
-  const { theme, toggleTheme } = useSettings();
+  const { setUser } = useAuth();
+  const { theme } = useSettings();
   const isDark = theme === "dark";
+
+  const handleThemeToggle = async () => {
+    try {
+      const res = await UserAPI.toggleUserTheme();
+      toast.success(res.data.message);
+      setUser(res.data.user);
+    } catch (error) {
+      console.error("Error toggling theme:", error);
+    }
+  };
 
   return (
     <div className="mt-6 space-y-4 animate-fadeIn">
@@ -14,24 +28,18 @@ const Appearance = () => {
       </h3>
 
       {/* Card */}
-      <div
-        onClick={toggleTheme}
-        className="bg-softWhite border border-gray-200 p-4 rounded-xl shadow-sm hover:shadow-md transition cursor-pointer flex items-center justify-between"
-      >
+      <div className="bg-softWhite border border-gray-200 p-4 rounded-xl shadow-sm hover:shadow-md transition cursor-pointer flex items-center justify-between">
         <div>
           <p className="font-medium text-charcoalGray">Dark Mode</p>
         </div>
 
         {/* Toggle Switch */}
-        <label
-          onClick={(e) => e.stopPropagation()}
-          className="relative inline-flex items-center cursor-pointer"
-        >
+        <label className="relative inline-flex items-center cursor-pointer">
           <input
             type="checkbox"
             className="sr-only peer"
             checked={isDark}
-            onChange={toggleTheme}
+            onClick={handleThemeToggle}
           />
           <div className="w-11 h-6 bg-tealGreen rounded-full peer peer-checked:bg-electricBlue transition-all" />
           <div className="absolute left-0.5 top-0.5 w-5 h-5 bg-white rounded-full shadow peer-checked:translate-x-5 transition-all" />
