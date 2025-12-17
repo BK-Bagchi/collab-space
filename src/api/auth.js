27d459c53a8a82c3215149e5d79def9ef6f85a65
unknown <bkbagchi.dipto@gmail.com>
2025-12-17 16:08:33 +0600

    api added to logout and all device logout

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,3 +4,5 @@ export const signup = (data) => axios.post("/auth/signup", data);
 export const login = (data) => axios.post("/auth/login", data);
 export const oauthLogin = (data) => axios.post("/auth/oauth", data);
 export const verifyToken = () => axios.get("/auth/verify");
+export const logout = () => axios.patch("/auth/logout");
+export const logoutAllDevices = () => axios.patch("/auth/logout/all");
